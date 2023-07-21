d78c7aecff953f15fa247c19f1f9ab751221db4b
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-07-20 21:58:19 -0400

    fixed dark colors and hidpi mode

--- gesture_drawing/__init__.py
+++ gesture_drawing/__init__.py
@@ -11,35 +11,11 @@ from qtpy import QtWidgets as QW
 from qtpy import QtGui as QG
 
 from .gui import GUIClient
+from .gui import GUICommon
 from .core import CoreLogging as logging
 from .core import CoreController
 from .core import CoreConstants as CC
 
-def get_darkModePalette( app=None ) :
-    
-    darkPalette = app.palette()
-    darkPalette.setColor(QG.QPalette.Window, QG.QColor( 53, 53, 53 ) )
-    darkPalette.setColor(QG.QPalette.WindowText,QC.Qt.white )
-    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.WindowText, QG.QColor( 127, 127, 127 ) )
-    darkPalette.setColor(QG.QPalette.Base, QG.QColor( 42, 42, 42 ) )
-    darkPalette.setColor(QG.QPalette.AlternateBase, QG.QColor( 66, 66, 66 ) )
-    darkPalette.setColor(QG.QPalette.ToolTipBase, QC.Qt.white )
-    darkPalette.setColor(QG.QPalette.ToolTipText, QC.Qt.white )
-    darkPalette.setColor(QG.QPalette.Text, QC.Qt.white )
-    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.Text, QG.QColor( 127, 127, 127 ) )
-    darkPalette.setColor(QG.QPalette.Dark, QG.QColor( 35, 35, 35 ) )
-    darkPalette.setColor(QG.QPalette.Shadow, QG.QColor( 20, 20, 20 ) )
-    darkPalette.setColor(QG.QPalette.Button, QG.QColor( 53, 53, 53 ) )
-    darkPalette.setColor(QG.QPalette.ButtonText, QC.Qt.white )
-    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.ButtonText, QG.QColor( 127, 127, 127 ) )
-    darkPalette.setColor(QG.QPalette.BrightText, QC.Qt.red )
-    darkPalette.setColor(QG.QPalette.Link, QG.QColor( 42, 130, 218 ) )
-    darkPalette.setColor(QG.QPalette.Highlight, QG.QColor( 42, 130, 218 ) )
-    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.Highlight, QG.QColor( 80, 80, 80 ) )
-    darkPalette.setColor(QG.QPalette.HighlightedText, QC.Qt.white )
-    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.HighlightedText, QG.QColor( 127, 127, 127 ), )
-    
-    return darkPalette
 
 def main():
     logging.setup_logging(CC.LOG_FILE)
@@ -50,9 +26,11 @@ def main():
     controller.boot_everything()
 
     try:
+        GUICommon.enable_hi_dpi()
+
         app = QW.QApplication([])
         app.setApplicationName("Gesture Drawing")
-        app.setPalette( get_darkModePalette(app))
+        app.setPalette( GUICommon.get_darkModePalette(app))
         
 
         mainwindow = GUIClient.ClientWindow(controller)
--- gesture_drawing/gui/GUICommon.py
+++ gesture_drawing/gui/GUICommon.py
@@ -8,6 +8,74 @@ from qtpy import QtWidgets as QW
 from qtpy import QtGui as QG
 
 
+def enable_hi_dpi() -> None:
+    """Allow to HiDPI.
+
+    This function must be set before instantiation of QApplication..
+    For Qt6 bindings, HiDPI “just works” without using this function.
+    """
+
+    if hasattr(QC.Qt.ApplicationAttribute, "AA_UseHighDpiPixmaps"):
+        QG.QGuiApplication.setAttribute(QC.Qt.ApplicationAttribute.AA_UseHighDpiPixmaps)
+
+    if hasattr(QC.Qt.ApplicationAttribute, "AA_EnableHighDpiScaling"):
+        QG.QGuiApplication.setAttribute(QC.Qt.ApplicationAttribute.AA_EnableHighDpiScaling)
+
+    if hasattr(QC.Qt, "HighDpiScaleFactorRoundingPolicy"):
+        os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
+        QG.QGuiApplication.setHighDpiScaleFactorRoundingPolicy(
+            QC.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
+        )
+
+def get_darkModePalette( app=None ) :
+
+    BACKGROUND = QG.QColor( 53, 53, 53 )
+    BACKGROUND_DARK = QG.QColor( 42, 42, 42 )
+    BACKGROUND_LIGHT = QG.QColor( 66, 66, 66 )
+
+    FOREGROUND = QG.QColor( 255, 255, 255 )
+    FOREGROUND_DISABLED = QG.QColor( 127, 127, 127 )
+
+    HIGHLIGHT = QG.QColor( 42, 130, 218 )
+    HIGHLIGHT_DISABLED = QG.QColor( 80, 80, 80 )
+
+    LINK = QG.QColor( 42, 130, 218 )
+    DARK = QG.QColor( 35, 35, 35 )
+    SHADOW = QG.QColor( 20, 20, 20 )
+    BRIGHT_TEXT = QC.Qt.red
+    
+    darkPalette = app.palette()
+
+    darkPalette.setColor(QG.QPalette.Window, BACKGROUND )
+    darkPalette.setColor(QG.QPalette.Base, BACKGROUND_DARK )
+    darkPalette.setColor(QG.QPalette.AlternateBase, BACKGROUND_LIGHT )
+
+    darkPalette.setColor(QG.QPalette.WindowText, FOREGROUND )
+    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.WindowText, FOREGROUND_DISABLED )
+
+    darkPalette.setColor(QG.QPalette.ToolTipBase, BACKGROUND )
+    darkPalette.setColor(QG.QPalette.ToolTipText, FOREGROUND )
+    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.ToolTipText, FOREGROUND_DISABLED )
+
+    darkPalette.setColor(QG.QPalette.Text, FOREGROUND )
+    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.Text, FOREGROUND_DISABLED )
+
+    darkPalette.setColor(QG.QPalette.Dark, DARK )
+    darkPalette.setColor(QG.QPalette.Shadow, SHADOW )
+
+    darkPalette.setColor(QG.QPalette.BrightText, BRIGHT_TEXT )
+    darkPalette.setColor(QG.QPalette.Link, LINK )
+    darkPalette.setColor(QG.QPalette.Highlight, HIGHLIGHT )
+    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.Highlight, HIGHLIGHT_DISABLED )
+
+    darkPalette.setColor(QG.QPalette.Button, BACKGROUND )
+    darkPalette.setColor(QG.QPalette.ButtonText, FOREGROUND )
+    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.ButtonText, FOREGROUND_DISABLED )
+
+    darkPalette.setColor(QG.QPalette.HighlightedText, FOREGROUND )
+    darkPalette.setColor(QG.QPalette.Disabled, QG.QPalette.HighlightedText, FOREGROUND_DISABLED )
+
+    return darkPalette
 
 class InfiniteProgressBar(QW.QProgressBar):
     def __init__(self, step_amount=10,timer_speed_ms=100):
